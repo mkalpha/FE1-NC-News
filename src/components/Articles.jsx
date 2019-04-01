23f2966742bf613da31b88acd3c306c67918621a
Andrew <andrewjworkman@googlemail.com>
2019-04-01 17:05:33 +0100

    Added fetch topics to get topics from API

--- src/components/Articles.jsx
+++ src/components/Articles.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Router, Link } from '@reach/router'
 import axios from 'axios';
+import '../styles/Articles.css'
 
 
 
@@ -10,13 +11,22 @@ class Articles extends Component {
     }
 
     render() {
-        return (
-            <div id="articlesWrapper">
-            Articles will go here
+        return (this.state.articlesData !== null && <div id="articlesWrapper">
+            Articles
+            <div>Sort by goes here</div>
             <ul>
-                <li>
-                   {console.log(this.state.articlesData)}
-                </li>
+              {this.state.articlesData.map(article => {
+                  return <li>
+                            <div className="articleListWarpper">
+                                <div className="articleListTitle"><b>Title: </b>{article.title}</div>
+                                <div className="articleListTopic"><b>Topic: </b>{article.topic}</div>
+                                <div className="articleListAuthor"><b>Author: </b>{article.author}</div>
+                                <div className="articleListCommentCount">Comments: {article.comment_count}</div>
+                                <div className="articleListVotes">Votes: {article.votes} </div>
+                                <div className="articleListCreatedAt">Created at: {article.created_at}</div>
+                            </div>
+                        </li>
+              })}
             </ul>
             </div>
         )
