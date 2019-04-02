18d129f4e04ef4e90c3c987eb5791372410b75f7
Andrew <andrewjworkman@googlemail.com>
2019-04-02 20:15:50 +0100

    Added hideable element to nav bar and sticky navbar

--- ToDo.md
+++ ToDo.md
@@ -2,6 +2,19 @@
 
 1. extract axios request to a separate api.js file 
 
-2. fix sort by bug with utils function - SortTopicsForm.jsx
+2. fix sort by bug with utils function and testing - SortTopicsForm.jsx
 
-3. extract votes Patch to a utils function as i will be using it again in comments
+3. extract votes Patch to a utils function as I will be using it again in comments
+
+4. sort out the date formatting on articles and comments
+
+
+
+
+# Check with Paul 
+
+1. in both Nav with the appearing form and Article with the votes I am not calling componentDidUpdate, am I causing more rendering than I need to 
+
+2. Am I over compartmentalising things? 
+
+3. Am I using too many class based components?
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,13 +1,30 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Link } from '@reach/router'
 import '../styles/Nav.css'
 
-const Nav = props => {
-    return ( <div id="navWrapper">
-    <div id ="logo"><Link to={'/'}>NC_News</Link></div>
-    <div id="login">Login</div>
-    </div>
-    )
+class Nav extends Component {
+    state = {
+        showAddArticle : false
+    }
+    render() {
+    return (<div id="nav"><div id="navWrapper">
+                    <div id ="logo"><Link to={'/'}>NC_News</Link></div>
+                    <div id="login">Login</div>
+                    <button onClick={this.handleClick}>Post New Article</button>
+            </div>
+            {this.state.showAddArticle !== false &&<form>
+                <h1>Add New Article</h1>
+            </form>}
+            </div> 
+              
+            
+        )
+    }
+
+
+    handleClick = (event) => {
+       this.state.showAddArticle === false ? this.setState({ showAddArticle : true}) : this.setState({ showAddArticle : false}) 
+    }
 }
-    
+
 export default Nav;
