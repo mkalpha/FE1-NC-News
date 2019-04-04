7e24605ef2ff608618341a5831a51a2e0d12d02d
Andrew <andrewjworkman@googlemail.com>
2019-04-04 10:27:55 +0100

    added add new topic component

---
+++ src/components/AddNewTopic.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class AddNewTopic extends Component {
+    state = {}
+
+    render () {
+        return ( <div id ="addNewTopicWrapper"> <p>Add New Topic</p> </div>
+
+        )
+    }
+}
+
+export default AddNewTopic
