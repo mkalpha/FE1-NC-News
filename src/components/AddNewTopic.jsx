bfe27b517216a0d3dccaa613ae7163590d72a1b3
Andrew <andrewjworkman@googlemail.com>
2019-04-04 10:51:01 +0100

    added addTopic Form

--- src/components/AddNewTopic.jsx
+++ src/components/AddNewTopic.jsx
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
+import '../styles/AddNewTopic.css'
 
 class AddNewTopic extends Component {
     state = {}
 
     render () {
-        return ( <div id ="addNewTopicWrapper"> <p>Add New Topic</p> </div>
 
-        )
-    }
+        return (<div id="addNewTopicWrapper">
+                    <form>
+                        Add Topic Name: <input></input>
+                        Add Topic Description <input></input>
+                        <button>Create Your New Topic >>></button>
+                    </form>
+                    <button>Cancel</button>
+        
+                </div>)
+        
+     }
 }
 
 export default AddNewTopic
