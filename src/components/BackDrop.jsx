1061aab8ff0cd026f71957e2269d9862b78da46b
Andrew <andrewjworkman@googlemail.com>
2019-04-30 21:05:51 +0100

    added toggle handler to backDrop

--- src/components/BackDrop.jsx
+++ src/components/BackDrop.jsx
@@ -2,8 +2,13 @@ import React from 'react';
 import '../styles/BackDrop.css'
 
 const BackDrop = props => {
+
+   const clickHandler = () => {
+        props.drawerToggleHandler()
+    }
+
     return (
-        <div className="backDrop" />
+        <div className="backDrop" onClick={clickHandler} />
         
     )
 }
